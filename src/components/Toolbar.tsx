c1718a42b1449a0609b7f4aa747bf58dd47b39dd
minagishl <mvxd568.co@icloud.com>
2025-01-04 19:28:58 +0900

    style: adjust element sizes

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -170,7 +170,9 @@ function Popover({ text }: { text: string }): React.ReactElement {
   return (
     <div className="absolute bg-white rounded-md shadow-md flex items-center justify-center left-1/2 -translate-x-1/2 top-2 animate-fade animate-once animate-duration-300">
       <div className="w-fit flex p-2 px-4 items-center justify-center font-sans flex-col whitespace-nowrap">
-        <p>{text}</p>
+        <p className="h-5 text-center items-center justify-center flex">
+          {text}
+        </p>
       </div>
     </div>
   );
