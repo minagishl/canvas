d3ea4478cef0b6776c00754e2c39cfa48a334cc9
minagishl <mvxd568.co@icloud.com>
2025-01-08 04:18:44 +0900

    chore: remove unused components

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Canvas } from './components/Canvas';
-import { Modal } from './components/Modal';
 import { AlertProvider } from './contexts/AlertContext';
 import { CanvasProvider } from './contexts/CanvasContext';
 import { Toolbar } from './components/Toolbar';
 
 function App(): React.ReactElement {
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
-
   return (
     <AlertProvider>
       <CanvasProvider>
         <div className="relative h-screen w-screen select-none overflow-hidden bg-gray-50">
           <Canvas />
           <Toolbar />
-          <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
         </div>
       </CanvasProvider>
     </AlertProvider>
