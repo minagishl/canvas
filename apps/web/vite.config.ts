1a65e1e3407babec2a4b9640e765860e2d812ea0
minagishl <mvxd568.co@icloud.com>
2025-01-15 04:57:49 +0900

    build: add HTML compression tool

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteMinifyPlugin({
+      removeComments: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: false,
+    }),
+  ],
   build: {
     minify: 'terser',
   },
