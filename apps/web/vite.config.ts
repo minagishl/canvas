e092fd595d5921be461450065b51f206669c62c4
minagishl <mvxd568.co@icloud.com>
2025-01-15 05:16:58 +0900

    feat: add analytics code

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
+    htmlPlugin(loadEnv(mode, '.')),
     ViteMinifyPlugin({
       removeComments: true,
       collapseWhitespace: true,
@@ -22,4 +23,15 @@ export default defineConfig({
     include: ['lucide-react'],
     exclude: ['lucide-react/icons'],
   },
-});
+}));
+
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: 'html-transform',
+    transformIndexHtml: {
+      order: 'pre' as const,
+      handler: (html: string): string =>
+        html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
+    },
+  };
+}
