f4c5fe496af83a413217eeaedfc5d71731e2cdba
minagishl <mvxd568.co@icloud.com>
2025-01-18 22:48:10 +0900

    feat: allow icon specification via arguments

--- apps/web/src/components/Modal.tsx
+++ apps/web/src/components/Modal.tsx
@@ -5,11 +5,41 @@ const navigateToUrl = (url: string) => {
   window.location.href = url;
 };
 
+type Items = {
+  icon: React.ReactNode;
+  text?: string;
+  onClick?: () => void;
+  close?: boolean;
+};
+
 interface ModalProps {
+  items?: Items[];
   close: () => void;
 }
 
-export const Modal = ({ close }: ModalProps) => {
+const defaultItems: Items[] = [
+  {
+    icon: <Github className="h-5 w-5" />,
+    text: 'GitHub',
+    onClick: () => navigateToUrl('https://github.com/minagishl/canvas'),
+  },
+  {
+    icon: <BookMarked className="h-5 w-5" />,
+    text: 'Guide',
+    onClick: () =>
+      navigateToUrl(
+        'https://github.com/minagishl/canvas/blob/main/docs/GUIDE.md'
+      ),
+  },
+  {
+    icon: <X className="h-5 w-5" />,
+    text: 'Close',
+    onClick: () => {},
+    close: true,
+  },
+];
+
+export const Modal = ({ items = defaultItems, close }: ModalProps) => {
   return (
     <>
       <div
@@ -17,43 +47,21 @@ export const Modal = ({ close }: ModalProps) => {
         onClick={close}
       />
       <div className="fixed left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform gap-2 rounded-xl bg-white p-2">
-        <div className="group relative">
-          <button
-            className="cursor-pointer rounded-md p-2 text-black transition-colors hover:bg-gray-100"
-            onClick={() => navigateToUrl('https://github.com/minagishl/canvas')}
-          >
-            <Github className="h-5 w-5" />
-          </button>
-          <div className="absolute left-1/2 top-full mt-2 hidden -translate-x-1/2 group-hover:block">
-            <Popover text="GitHub" upper={false} />
+        {items.map((item, index) => (
+          <div key={index} className="group relative">
+            <button
+              className="cursor-pointer rounded-md p-2 text-black transition-colors hover:bg-gray-100"
+              onClick={item.close ? close : item.onClick}
+            >
+              {item.icon}
+            </button>
+            {item.text && (
+              <div className="absolute left-1/2 top-full mt-2 hidden -translate-x-1/2 group-hover:block">
+                <Popover text={item.text} upper={false} />
+              </div>
+            )}
           </div>
-        </div>
-        <div className="group relative">
-          <button
-            className="cursor-pointer rounded-md p-2 text-black transition-colors hover:bg-gray-100"
-            onClick={() =>
-              navigateToUrl(
-                'https://github.com/minagishl/canvas/blob/main/docs/GUIDE.md'
-              )
-            }
-          >
-            <BookMarked className="h-5 w-5" />
-          </button>
-          <div className="absolute left-1/2 top-full mt-2 hidden -translate-x-1/2 group-hover:block">
-            <Popover text="Guide" upper={false} />
-          </div>
-        </div>
-        <div className="group relative">
-          <button
-            className="cursor-pointer rounded-md p-2 text-black transition-colors hover:bg-gray-100"
-            onClick={close}
-          >
-            <X className="h-5 w-5" />
-          </button>
-          <div className="absolute left-1/2 top-full mt-2 hidden -translate-x-1/2 group-hover:block">
-            <Popover text="Close" upper={false} />
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
