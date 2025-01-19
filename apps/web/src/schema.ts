e261748b52d188be1a1a7cc2009b8638e9d3e10c
minagishl <mvxd568.co@icloud.com>
2025-01-19 21:22:23 +0900

    fix: add missing key

--- apps/web/src/schema.ts
+++ apps/web/src/schema.ts
@@ -14,10 +14,18 @@ export const CanvasObjectSchema = v.object({
   height: v.number(),
   fill: v.optional(v.string()),
   text: v.optional(v.string()),
+  imageData: v.optional(v.string()),
   weight: v.optional(v.number()),
+  fontSize: v.optional(v.number()),
+  locked: v.optional(v.boolean()),
   points: v.optional(v.array(PointSchema)),
+  lineWidth: v.optional(v.number()),
+  italic: v.optional(v.boolean()),
+  rotation: v.optional(v.number()),
+  originalUrl: v.optional(v.string()),
+  circle: v.optional(v.boolean()),
+  spoiler: v.optional(v.boolean()),
   embedUrl: v.optional(v.string()),
-  src: v.optional(v.string()),
 });
 
 export const CanvasDataSchema = v.object({
