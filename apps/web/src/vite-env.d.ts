99e42fbf04cb8002a05263d0e4fb1f3a4c505137
minagishl <mvxd568.co@icloud.com>
2025-02-01 00:01:54 +0900

    feat: add comment to clarify environment variable purpose

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -5,7 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_EXAMPLE_CANVAS_URL: string;
   readonly VITE_STATUS_API_URL: string;
 
-  //
+  // Environment variable to enable/disable
   readonly VITE_ENABLED_GIF: string;
   readonly VITE_ENABLED_RESIZES_NAP: string;
   readonly VITE_ENABLED_PRESENTATION: string;
