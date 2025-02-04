f421fed858e1e88a1e6afe0e122f4d72c24024b4
minagishl <mvxd568.co@icloud.com>
2025-02-04 10:39:15 +0900

    fix: resolve issue preventing duplicate image attachments

--- apps/web/src/components/Canvas.tsx
+++ apps/web/src/components/Canvas.tsx
@@ -937,6 +937,7 @@ export const Canvas = () => {
       setCurrentHistoryIndex,
       currentHistoryIndex,
     });
+    fileInputRef.current!.value = '';
   };
 
   // Delete an object with the Delete key
