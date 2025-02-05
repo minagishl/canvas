07004129c5f7845b15e38a50fac74d92d7b263df
minagishl <mvxd568.co@icloud.com>
2025-02-05 17:34:22 +0900

    feat: add react-helmet-async for SEO enhancements

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { Canvas } from '~/components/Canvas';
 import { Toolbar } from '~/components/Toolbar';
 
+// SEO
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+const schema = {
+  '@context': 'https://schema.org',
+  '@type': 'WebApplication',
+  name: 'Canvas',
+  description: 'A free canvas tool available online for building ideas.',
+  applicationCategory: 'DesignApplication',
+  operatingSystem: 'All',
+};
+
 // Contexts
 import { AIProvider } from './contexts/AIContext';
 import { AlertProvider } from '~/contexts/AlertContext';
@@ -10,18 +22,25 @@ import { HistoryProvider } from '~/contexts/HistoryContext';
 
 function App(): React.ReactElement {
   return (
-    <AlertProvider>
-      <HistoryProvider>
-        <CanvasProvider>
-          <AIProvider>
-            <div className="relative h-screen w-screen overflow-hidden bg-gray-50 select-none">
-              <Canvas />
-              <Toolbar />
-            </div>
-          </AIProvider>
-        </CanvasProvider>
-      </HistoryProvider>
-    </AlertProvider>
+    <div>
+      <HelmetProvider>
+        <Helmet>
+          <script type="application/ld+json">{JSON.stringify(schema)}</script>
+        </Helmet>
+      </HelmetProvider>
+      <AlertProvider>
+        <HistoryProvider>
+          <CanvasProvider>
+            <AIProvider>
+              <div className="relative h-screen w-screen overflow-hidden bg-gray-50 select-none">
+                <Canvas />
+                <Toolbar />
+              </div>
+            </AIProvider>
+          </CanvasProvider>
+        </HistoryProvider>
+      </AlertProvider>
+    </div>
   );
 }
 
